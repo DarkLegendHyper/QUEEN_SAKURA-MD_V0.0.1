db6d05cb1a9135fe0e103e3e2c0756912338e582
DARK-LEGEND_HYPER <144076302+DarkLegendHyper@users.noreply.github.com>
2023-09-12 16:37:24 +0530

    Update img-asupan.js

--- plugins/img-asupan.js
+++ plugins/img-asupan.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, usedPrefix, command }) => {
 	
     let img = await conn.getFile(global.API('fgmods', '/api/img/asupan-la', { }, 'apikey'))
     let asupan = img.data
-    conn.sendFile(m.chat, asupan, 'vid.mp4', `✅ Resultado`, m)
+    conn.sendFile(m.chat, asupan, 'vid.mp4', `🔱 *BY 𝑄𝑈𝛯𝛯𝛮-𝑆𝛥𝛫𝑈𝑅𝛥_𝛭𝐷*`, m)
     m.react(dmoji)
     
 }
