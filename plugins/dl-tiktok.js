82ad81fc1823877766f3be80831be940e944fcc4
DARK-LEGEND_HYPER <144076302+DarkLegendHyper@users.noreply.github.com>
2023-09-12 16:56:25 +0530

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -2,33 +2,33 @@
 import fg from 'api-dylux' 
 import { tiktokdl } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, args, usedPrefix, command}) => {
-if (!args[0]) throw `✳️ Ingrese un link de Tiktok\n\n 📌 Ejemplo : ${usedPrefix + command} https://vm.tiktok.com/ZMYG92bUh/`
-if (!args[0].match(/tiktok/gi)) throw `❎ verifica que el link sea de tiktok`
+if (!args[0]) throw `🔱 Enter the Tiktok Url\n\n 🌿 Example : ${usedPrefix + command} https://vm.tiktok.com/ZMYG92bUh/`
+if (!args[0].match(/tiktok/gi)) throw `❎  This Link is Not value`
 m.react(rwait)
 
 try {
     let p = await fg.tiktok(args[0]) 
     let te = `
-┌─⊷ TIKTOK
-▢ *Nombre:* ${p.nickname}
-▢ *Username:* ${p.unique_id}
-▢ *Duración:* ${p.duration}
-▢ *Descripción:* ${p.description}
-└───────────`
+┌⚋⚋⚋⚋❲ *TIKTOK* ❳⚋⚋⚋⦁❉⦁
+⦁ 🌿 *NAME:* ${p.nickname}
+⦁ ⚡ *Username:* ${p.unique_id}
+⦁ ⏳ *Duration:* ${p.duration}
+⦁ 🔖 *Description:* ${p.description}
+└───────────❉⦁⦁`
     conn.sendFile(m.chat, p.play, 'tiktok.mp4', te, m)
     m.react(done)
     } catch {  	
     try { 
 	const { author: { nickname }, video, description } = await tiktokdl(args[0])
     const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
-    if (!url) throw '❎ Error al descargar el video'
+    if (!url) throw '❎ Error...'
     conn.sendFile(m.chat, url, 'fb.mp4', `
-┌─⊷ *TIKTOK DL-2*
-▢ *Nickname:* ${nickname} ${description ? `\n▢ *Descripción:* ${description}` : ''}
-└───────────`, m)
+┌⚋⚋⚋⚋ ❲ *TIKTOK-2* ❳⚋⚋⚋⦁❉⦁
+⦁🔱 *Nickname:* ${nickname} ${description ? `\n⦁ 🔖 *Description:* ${description}` : ''}
+└───────────❉⦁⦁`, m)
 m.react(done)
 } catch {
-    m.reply(`❎ Error al descargar el video`)
+    m.reply(`❎ Error....`)
 }
 } 
     
@@ -36,6 +36,6 @@ m.react(done)
 handler.help = ['tiktok']
 handler.tags = ['dl']
 handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm)$/i
-handler.diamond = true
+handler.diamond = false
 
 export default handler
