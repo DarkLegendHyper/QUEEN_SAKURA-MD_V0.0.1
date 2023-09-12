29e1750c99bcfc12dfd783c2a63974b24f38436d
DARK-LEGEND_HYPER <144076302+DarkLegendHyper@users.noreply.github.com>
2023-09-12 16:31:45 +0530

    Update tools-wikipedia.js

--- plugins/tools-wikipedia.js
+++ plugins/tools-wikipedia.js
@@ -3,20 +3,20 @@ import cheerio from 'cheerio'
 
 
 let handler = async (m, { text }) => {
-	if (!text) throw `✳️ Ingrese lo que quiere buscar en Wikipedia` 
+	if (!text) throw `🌿 please use en Wikipedia` 
 	
     try {
 	const link =  await axios.get(`https://es.wikipedia.org/wiki/${text}`)
 	const $ = cheerio.load(link.data)
 	let wik = $('#firstHeading').text().trim()
 	let resulw = $('#mw-content-text > div.mw-parser-output').find('p').text().trim()
-	m.reply(`▢ *Wikipedia*
+	m.reply(`⦁⚋⚋⚋❲ *Wikipedia* ❳⚋⚋⚋⦁
 
-‣ Buscado : ${wik}
+⦁ Tool : ${wik}
 
 ${resulw}`)
 } catch (e) {
-  m.reply('⚠️ No se han encontrado resultados ')
+  m.reply('⚠️ Not have ')
 }
 }
 handler.help = ['wikipedia']
