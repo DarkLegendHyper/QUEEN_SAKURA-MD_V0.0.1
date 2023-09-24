593ca4ab442e091dfdaa248cdb95b27b8c37ca72
DARK-LEGEND_HYPER <144076302+DarkLegendHyper@users.noreply.github.com>
2023-09-24 20:43:35 +0530

    Update _expired-gp.js

--- plugins/_expired-gp.js
+++ plugins/_expired-gp.js
@@ -6,7 +6,7 @@ export async function all(m) {
     if (!chats.expired)
         return !0
     if (+new Date() > chats.expired) {
-        await this.reply(m.chat, `🔴 Adiós shavales  *${this.user.name}* saldrá del grupo \n\nfinalizo su alquiler`)
+        await this.reply(m.chat, `⚡ GOOD BYE I AM LEAVE NOW.. *${this.user.name}*Expired  \n\nend.....`)
         await this.groupLeave(m.chat)
         chats.expired = null
     }
