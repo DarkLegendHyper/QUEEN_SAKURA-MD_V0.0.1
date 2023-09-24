70b9f719fed426790576163e0c4c647187683d7b
DARK-LEGEND_HYPER <144076302+DarkLegendHyper@users.noreply.github.com>
2023-09-24 20:25:16 +0530

    Update img-gimage.js

--- plugins/img-gimage.js
+++ plugins/img-gimage.js
@@ -1,10 +1,10 @@
 
 import fg from 'api-dylux'
 let handler  = async (m, { conn, args, text, usedPrefix, command }) => {
-  if (!text) throw `✳️ Ingrese la imagen que quiere buscar \n\n📌 Ejemplo: *${usedPrefix + command}* Billie Eilish`
+  if (!text) throw `⚡ Please Enter the Text \n\n👉 Example: *${usedPrefix + command}* *QUEEN-SAKURA*`
   let res = await fg.googleImage(text)
   conn.sendFile(m.chat, res.getRandom(), 'img.png', `
-✅ Resultado de : *${text}*`.trim(), m)
+✅ *Image Name* : *${text}*\n💃BY 𝑄𝑈𝛯𝛯𝛮-𝑆𝛥𝛫𝑈𝑅𝛥_𝛭𝐷`.trim(), m)
 }
 handler.help = ['imagen']
 handler.tags = ['img']
