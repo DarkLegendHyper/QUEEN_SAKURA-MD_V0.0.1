738d6ebfa1fc0ee2c0fb828c40ade961a39ef2e6
DARK-LEGEND_HYPER <144076302+DarkLegendHyper@users.noreply.github.com>
2023-09-24 20:29:22 +0530

    Update img-wallpaper.js

--- plugins/img-wallpaper.js
+++ plugins/img-wallpaper.js
@@ -1,13 +1,13 @@
 
 import fg from 'api-dylux';
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) throw `✳️ Que fondo de pantalla busco?`
+  if (!text) throw `⚡ Enter the Text`
   try {
     let res = await fg.wallpaper(text);
     let re = pickRandom(res);
-    await conn.sendMessage(m.chat, { image: { url: re.image[0] }, caption: `✅ Genial no?` }, { quoted: m });
+    await conn.sendMessage(m.chat, { image: { url: re.image[0] }, caption: `💃BY QUEEN-SAKURA` }, { quoted: m });
   } catch (error) {
-   m.reply(`✳️ Error: intenta más tarde`)
+   m.reply(`⚡ Error:.....`)
   }
   
 }
