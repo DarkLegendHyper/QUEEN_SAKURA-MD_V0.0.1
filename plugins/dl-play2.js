776fdc72f0157469f8b1a846c689e65d7d73efe0
DARK-LEGEND_HYPER <144076302+DarkLegendHyper@users.noreply.github.com>
2023-11-24 17:50:30 +0530

    Update dl-play2.js

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -2,14 +2,14 @@
 import yts from 'yt-search'
 import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
 let limit = 320
-let handler = async (m, { conn, text, args, isPrems, usedPrefix, command }) => {
+let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
   
-    if (!text) throw `👸 Enter a song title\n\n📌 Example *${usedPrefix + command}* *Baby Dont go Baby*`
+    if (!text) throw `✳️ Ingresa el título de una canción\n\n📌Ejemplo *${usedPrefix + command}* Lil Peep hate my life`
   let chat = global.db.data.chats[m.chat]
   let res = await yts(text)
   //let vid = res.all.find(video => video.seconds < 3600)
   let vid = res.videos[0]
-  if (!vid) throw `👸 Vídeo/Audio no found`
+  if (!vid) throw `✳️ Vídeo/Audio no encontrado`
   let isVideo = /vid$/.test(command)
   m.react('🎧') 
   
@@ -20,36 +20,26 @@ let handler = async (m, { conn, text, args, isPrems, usedPrefix, command }) => {
   let dl_url = await (isVideo ? yt.video[q].download() : yt.audio[q].download())
   let title = await yt.title
   let size = await (isVideo ? yt.video[q].fileSizeH : yt.audio[q].fileSizeH)
-  let play = `
-┏─── 〔 Y O U T U B E 〕 ─⦁
-┌──────────────
-⦁ 📌 *Tile* : ${title}
-⦁ 📆 *Relise:* ${ago}
-⦁ ⌚ *duration:* ${timestamp}
-⦁ 👀 *views:* ${views}
-└──────────────⦁⦁`
-*Downloading And Uploading Song*`
-conn.sendFile(m.chat, vid.thumbnail, 'song', play, m, null, rpig)
+  let play = `_*DOWNLOADING ${vid.title} | RELISE ON ${vid.ago} |DURATION ${vid.timestamp}*_`
+conn.sendFile(m.chat, vid.thumbnail, 'play', play, m, null, rpig)
 
-if (size.split('MB')[0] >= limit) return m.reply(`*YT-DL*\n\n⦁ *⚖️Size* : ${size}\n⦁ *🎞️quality* : ${q}\n\n⦁ _The file exceeds the download limit_ *+${limit} MB*`) 
-if (size.includes('GB')) return m.reply(`*YT-DL*\n\n⦁ *⚖️Size* : ${size}\n⦁ *🎞️quality* : ${q}\n\n⦁ _File exceeds download limit_ *+${limit} MB*`)   
+if (size.split('MB')[0] >= limit) return m.reply(` ≡  *FG YTDL*\n\n▢ *⚖️Peso* : ${size}\n▢ *🎞️Calidad* : ${q}\n\n▢ _El archivo supera el límite de descarga_ *+${limit} MB*`) 
+if (size.includes('GB')) return m.reply(` ≡  *FG YTDL*\n\n▢ *⚖️Peso* : ${size}\n▢ *🎞️Calidad* : ${q}\n\n▢ _El archivo supera el límite de descarga_ *+${limit} MB*`)   
 	  conn.sendFile(m.chat, dl_url, title + '.mp' + (3 + /vid$/.test(command)), `
-┏─── 〔 Y O U T U B E 〕 ─⦁
-┣❍ 
-┣❍ *🎀Tile* : ${title}
-┣❍ *🎞️quality* : ${q}
-┣❍ *⚖️Size* : ${size}
-┣❍ ©ʙʏ Qᴜᴇᴇɴ-ꜱᴀᴋᴜʀᴀ
-┗━━━━━━━❍
-`.trim(), m, false, { mimetype: isVideo ? '' : 'audio/mpeg', asaudio: chat.useaudio })
+ ≡  *SAKURA-YTDL*
+  
+▢ *TITLE* : ${title}
+▢ *QUALITY* : ${q}
+▢ *⚖️SIZE* : ${size}
+`.trim(), m, false, { mimetype: isVideo ? '' : 'audio/mpeg', asDocument: chat.useDocument })
 		m.react(done) 
     } catch {
-		m.reply(`Error: 🥺`)
+		m.reply(`Error: intenta de nuevo`)
     }
 
 }
 handler.help = ['play']
 handler.tags = ['dl']
-handler.command = ['play', 'song']
+handler.command = ['play', 'playvid']
 
 export default handler
